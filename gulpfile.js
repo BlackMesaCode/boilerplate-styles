d3abf9787e62b4da0a2fd260e90d5773b2ef6d5f
Stephan Sum <sum@live.de>
2019-08-29 16:16:46 +0200

    Updated npm packages; Removed Bower

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,25 @@ var rename = require("gulp-rename");
 var del = require('del');
 var exec = require('child_process').exec; // part of nodejs - no npm package needed
 
+
+
+var liveServer = require("live-server");
+ 
+var liveServerParams = {
+    port: 8181, // Set the server port. Defaults to 8080.
+    host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+    root: "dist", // Set root directory that's being served. Defaults to cwd.
+    open: true, // When false, it won't load your browser by default.
+    ignore: 'scss,my/templates', // comma-separated string for paths to ignore
+    file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+    wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+    mount: [['/components', './node_modules']], // Mount a directory to a route.
+    logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+    middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+};
+
+
+
 gulp.task("sass", function() {
     return gulp
         .src("./styles/*.scss")
@@ -38,11 +57,12 @@ gulp.task("clean", function () {
 
 
 gulp.task('liveserver', function (cb) {
-    exec('live-server dist', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-    });
+    liveServer.start(liveServerParams);
+    // exec('live-server dist', function (err, stdout, stderr) {
+    //     console.log(stdout);
+    //     console.log(stderr);
+    //     cb(err);
+    // });
 });
 
 
